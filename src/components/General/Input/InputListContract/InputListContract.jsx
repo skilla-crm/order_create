0bb695011beeb7027a73963a7cc24ec820fa5b8e
alexholm222 <alexholm222@yandex.ru>
2025-12-10 17:19:52 +0300

    fix

--- src/components/General/Input/InputListContract/InputListContract.jsx
+++ src/components/General/Input/InputListContract/InputListContract.jsx
@@ -48,7 +48,7 @@ const InputListContract = ({ vis, list, value, setValue, width }) => {
 
             <div ref={fieldRef} onClick={handleOpenList} className={s.field}>
                 <div className={s.contract}>
-                    <p>{`${value?.template_name} №${value?.prefix ? ' ' + value?.prefix : ''} ${value?.number}`} {value?.date ? ` от ${dayjs(value?.date).format('DD.MM.YYYY')}` : ''}</p>
+                    <p>{`${value?.template_name ? value?.template_name : 'Договор'} №${value?.prefix ? ' ' + value?.prefix : ''} ${value?.number}`} {value?.date ? ` от ${dayjs(value?.date).format('DD.MM.YYYY')}` : ''}</p>
                     <span>{`${value?.partnership_name} ${value?.partnership_details?.bank ? value?.partnership_details?.bank : ''} ${value?.partnership_details?.rs ? `*${value?.partnership_details?.rs.slice(-4)}` : ''}`}</span>
                 </div>
                 <IconChewron className={classNames(s.chewron, openList && s.chewron_up)} />
@@ -61,7 +61,7 @@ const InputListContract = ({ vis, list, value, setValue, width }) => {
                     key={el.id}
                     id={el.id}
                 >
-                    <p>{`${el?.template_name} №${el?.prefix ? ' ' + el?.prefix : ''} ${el?.number}`} {el?.date ? ` от ${dayjs(el?.date).format('DD.MM.YYYY')}` : ''}</p>
+                    <p>{`${el?.template_name ? el?.template_name : 'Договор'} №${el?.prefix ? ' ' + el?.prefix : ''} ${el?.number}`} {el?.date ? ` от ${dayjs(el?.date).format('DD.MM.YYYY')}` : ''}</p>
                     <span>{`${el?.partnership_name} ${el?.partnership_details?.bank ? el?.partnership_details?.bank : ''} ${el?.partnership_details?.rs ? `*${el?.partnership_details?.rs.slice(-4)}` : ''}`}</span>
                 </li>)}
             </ul>
