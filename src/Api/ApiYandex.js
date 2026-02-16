7c978c32f34b7b38d9cd11b0e74e0f0319478d12
alexholm222 <alexholm222@yandex.ru>
2026-02-16 18:37:14 +0300

    fix/env

--- src/Api/ApiYandex.js
+++ src/Api/ApiYandex.js
@@ -8,7 +8,6 @@ const API_KEY_GEOSADGEST = process.env.REACT_APP_API_KEY_GEOSADGEST;
 const instanceWithToken = axios.create({
     withCredentials: false,
 })
-
 export const getAddressSuggest = (query, defaultCordinate) => {
     return instanceWithToken.get(`${URL_YANDEX}v1/suggest?apikey=${API_KEY_GEOSADGEST}&&print_address=1&text=${query}&results=5&lang=ru&attrs=uri&types=geo,street,district,locality,area,province,house,metro&ll=${defaultCordinate[1]},${defaultCordinate[0]}`);
 }
